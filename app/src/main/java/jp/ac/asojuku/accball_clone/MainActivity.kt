eb674d3af49df4f87f813eda0959a1d5ec62eb21
aso1901102 <1901102@s.asojuku.ac.jp>
2019-11-18 12:33:17 +0900

    ver2.0

--- app/src/main/java/jp/ac/asojuku/accball_clone/MainActivity.kt
+++ app/src/main/java/jp/ac/asojuku/accball_clone/MainActivity.kt
@@ -1,22 +1,58 @@
 package jp.ac.asojuku.accball_clone
 
 import android.content.Context
+import android.graphics.SurfaceTexture
 import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
 import android.os.Bundle
 import android.util.Log
+import android.view.SurfaceHolder
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity()
-        , SensorEventListener {
+        , SensorEventListener, //センサーの反応を受け取るためのインターフェース
+        SurfaceHolder.Callback{     //Surfaceviewを実装するのための窓口Holderのコールバックインターフェース
 
-    //connect-test1
+    //instance property
+    //SurfaceViewの幅と高さの初期値を設定
+    private var surfaceWidth:Int = 0;
+    private var surfaceHeight:Int = 0;
+
+    //ボールの半径
+    private val radius = 50.0f;
+
+    //ボールの移動量を計算するための係数
+    private val coef = 1000.0f;
+
+    //ボールの座標
+    //X座標
+    private var ballX:Float = 0f;
+
+    //Y座標
+    private var ballY:Float = 0f;
+
+    //X座標の重力加速度
+    private var vx:Float = 0f;
+
+    //Y座標の重力加速度
+    private var vy:Float = 0f;
+
+    //前回の時間を記録する変数
+    private var time:Long = 0L;
+
     // 誕生時のライフサイクルイベント
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main)//画面レイアウトを設定
+
+        //SurfaceHolderをView部品から取得
+        val holder = surfaceView.holder
+
+        //SurfaceHolderのコールバックに自クラスへの通知を追加
+        holder.addCallback(this);
     }
 
     // 画面表示・再表示のライフサイクルイベント
@@ -24,29 +60,12 @@ class MainActivity : AppCompatActivity()
         // 親クラスのonResume()処理
         super.onResume()
         // 自クラスのonResume()処理
-        // センサーマネージャをOSから取得
-        val sensorManager =
-                this.getSystemService(Context.SENSOR_SERVICE) as
-                        SensorManager;
-        // 加速度センサー(Accelerometer)を指定してセンサーマネージャからセンサーを取得
-        val accSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-        // リスナー登録して加速度センサーの監視を開始
-        sensorManager.registerListener(
-                this,  // イベントリスナー機能をもつインスタンス（自クラスのインスタンス）
-                accSensor, // 監視するセンサー（加速度センサー）
-                SensorManager.SENSOR_DELAY_GAME // センサーの更新頻度
-        )
+
     }
 
     // 画面が非表示の時のライフサイクルイベント
     override fun onPause() {
         super.onPause()
-        // センサーマネージャを取得
-        val sensorManager =
-                this.getSystemService(Context.SENSOR_SERVICE) as
-                        SensorManager;
-        // センサーマネージャに登録したリスナーを解除（自分自身を解除）
-        sensorManager.unregisterListener(this);
 
     }
 
@@ -71,5 +90,40 @@ class MainActivity : AppCompatActivity()
             // デバッグログに出力
             Log.d("加速度センサー", str);
         }
+    }
+
+    //Surfaceが生成された時のイベントに反応して呼ばれるコールバック
+    //画面が表示状態になった時のイベント処理
+    override fun surfaceCreated(holder: SurfaceHolder?) {
+        // センサーマネージャをOSから取得
+        val sensorManager =
+            this.getSystemService(Context.SENSOR_SERVICE) as
+                    SensorManager;
+        // 加速度センサー(Accelerometer)を指定してセンサーマネージャからセンサーを取得
+        val accSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        // リスナー登録して加速度センサーの監視を開始
+        sensorManager.registerListener(
+            this,  // イベントリスナー機能をもつインスタンス（自クラスのインスタンス）
+            accSensor, // 監視するセンサー（加速度センサー）
+            SensorManager.SENSOR_DELAY_GAME // センサーの更新頻度
+        )
+    }
+
+    //Surfaceが更新されたときにイベントに反応して呼ばれるコールバック
+    override fun surfaceChanged(holder: SurfaceHolder?, format: Int, width: Int, height: Int) {
+        //Surfaceが変化するたびに幅と高さを設定
+        this.surfaceWidth = width;
+        this.surfaceHeight = height;
+    }
+
+    //Surfaceが破棄されたときにイベントに反応して呼ばれるコールバック
+    //画面を非表示になった時のイベント処理
+    override fun surfaceDestroyed(holder: SurfaceHolder?) {
+        // センサーマネージャを取得
+        val sensorManager =
+            this.getSystemService(Context.SENSOR_SERVICE) as
+                    SensorManager;
+        // センサーマネージャに登録したリスナーを解除（OFFにする）
+        sensorManager.unregisterListener(this)
     }
 }
